3bab388f464172e1f5446b4ad0605da4060fed60
masonherhusky <mason.herhusky@gmail.com>
2014-10-18 03:06:44 -0400

    Added home page and log in page to android app

---
+++ src/com/purdueplaza/MyActivity.java
@@ -0,0 +1,41 @@
+package com.purdueplaza;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
+
+public class MyActivity extends Activity {
+
+    private EditText password;
+    private Button submit_button;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button register_button = (Button) findViewById(R.id.register_button);
+        register_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //    setContentView();
+            }
+        });
+        Button log_in_button = (Button) findViewById(R.id.log_in_button);
+        log_in_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                 logIn(v);
+            }
+        });
+
+    }
+
+    public void logIn(View view) {
+        Intent intent = new Intent(this, LogIn.class);
+        startActivity(intent);
+    }
+}
