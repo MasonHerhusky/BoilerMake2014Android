d3c90ea8b655a41e3a2cc743f404763d430107ab
masonherhusky <mason.herhusky@gmail.com>
2014-10-18 18:53:40 -0400

    working on homeActivity

--- src/com/purdueplaza/HomeActivity.java
+++ src/com/purdueplaza/HomeActivity.java
@@ -1,22 +1,20 @@
 package com.purdueplaza;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.*;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.app.ListActivity;
 import android.widget.AdapterView.OnItemClickListener;
 
-
 import java.util.ArrayList;
 
-import android.widget.Toast;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -27,12 +25,16 @@ import org.json.JSONObject;
 public class HomeActivity extends Activity{
 
     private ListView list;
+    final ArrayList<String> names = new ArrayList<String>();
+    final ArrayList<String> descriptions = new ArrayList<String>();
+    private Button register_event_button;
+    private Button search_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         list = (ListView) findViewById(R.id.listView);
-        ArrayList<String> values = new ArrayList<String>();
+
         RequestParams params = new RequestParams();
         params.put("page", 1);
         AsyncHttpClient client = new AsyncHttpClient();
@@ -42,7 +44,16 @@ public class HomeActivity extends Activity{
                 try {
                     JSONObject obj = new JSONObject(response);
                     if (obj.getBoolean("error") == false) {
-                        System.out.println(obj.toString());
+                        JSONArray events = obj.getJSONArray("events");
+                        for(int i = 0; i < events.length(); i++) {
+                            JSONObject c = events.getJSONObject(i);
+                            String name = c.getString("name");
+                            String description = c.getString("desc");
+                            System.out.println ("name" + i + " " + name);
+                            names.add(name);
+                            System.out.println ("description" + i + " " + description);
+                            descriptions.add(description);
+                        }
                     } else {
                         Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
                     }
@@ -52,6 +63,7 @@ public class HomeActivity extends Activity{
 
                 }
             }
+
 
             @Override
             public void onFailure(int statusCode, Throwable error, String content) {
@@ -64,28 +76,67 @@ public class HomeActivity extends Activity{
                 }
             }
         });
-    }
 
-
-
-      /*  ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, values);
-        setListAdapter(adapter);
+        ArrayAdapter<String> adapter = new Adapter(this, android.R.layout.simple_list_item_1, names);
+        list.setAdapter(adapter);
 
         list.setOnItemClickListener(new OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                Toast.makeText(getApplicationContext(),
-                        "Click ListItem Number " + position, Toast.LENGTH_LONG)
-                        .show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                clickedEvent(id);
+            }
+        });
+
+
+
+    }
+
+    public void addListenerOnButton() {
+
+        register_event_button = (Button) findViewById(R.id.register_button);
+
+        register_event_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                register(v);
+
             }
 
         });
     }
-*/
+    public void addListenerOnButton() {
+
+        search_button = (Button) findViewById(R.id.search_button);
+
+        search_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                searchEvents(v);
+
+            }
+
+        });
+    }
+
+    public void register(){
+        Intent registerIntent = new Intent(getApplicationContext(),RegisterEvent.class);
+        startActivity(registerIntent);
+    }
+
+
     public void searchEvents() {
 
+    }
+
+    public void clickedEvent(long id) {
+        Intent registerIntent = new Intent(getApplicationContext(),Event.class);
+        startActivity(registerIntent);
+        //TODO store event ID in intent to send it to Event activity
     }
 
 
