b25717a25bacfad6ea2a18e60a4c2644d3d94da9
masonherhusky <mason.herhusky@gmail.com>
2014-10-19 06:07:08 -0400

    Changed Start Date/Time and End Date/Time in RegisterEvent from EditText field to Calender buttons. However, we are still having difficulty displaying the date and time before the event is submitted... Also, the event is taking longer to create than before.

--- src/com/purdueplaza/RegisterEvent.java
+++ src/com/purdueplaza/RegisterEvent.java
@@ -1,5 +1,6 @@
 package com.purdueplaza;
 
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.SharedPreferences;
 import org.json.JSONException;
@@ -16,6 +17,7 @@ import android.widget.Button;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+
 /**
  *
  * Register Activity Class
@@ -24,9 +26,16 @@ public class RegisterEvent extends Activity {
 
     private EditText name;
     private EditText desc;
-    private EditText start;
-    private EditText end;
     private Button register_button;
+    private Button startTime_button;
+    private Button endTime_button;
+    private String start = "";
+    private String end = "";
+
+    DatePickerFragment startDateFragment = new DatePickerFragment();
+    DatePickerFragment endDateFragment = new DatePickerFragment();
+    TimePickerFragment startTimeFragment = new TimePickerFragment();
+    TimePickerFragment endTimeFragment = new TimePickerFragment();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +43,6 @@ public class RegisterEvent extends Activity {
         setContentView(R.layout.register_event);
         name = (EditText) findViewById(R.id.name_field);
         desc = (EditText) findViewById(R.id.description_field);
-        start = (EditText) findViewById(R.id.startTime_field);
-        end = (EditText) findViewById(R.id.endTime_field);
         addListenerOnButton();
     }
 
@@ -49,7 +56,41 @@ public class RegisterEvent extends Activity {
             }
 
         });
+        startTime_button = (Button) findViewById(R.id.startTime_button);
+        startTime_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showStartTimePickerDialog(view);
+                showStartDatePickerDialog(view);
+            }
+        });
+        endTime_button = (Button) findViewById(R.id.endTime_button);
+        endTime_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showEndTimePickerDialog(view);
+                showEndDatePickerDialog(view);
+            }
+        });
+
     }
+    public void showStartDatePickerDialog(View v) {
+        startDateFragment.show(this.getFragmentManager(), "datePicker");
+    }
+
+    public void showStartTimePickerDialog(View v) {
+        startTimeFragment.show(this.getFragmentManager(), "timePicker");
+    }
+
+    public void showEndTimePickerDialog(View v) {
+        endTimeFragment.show(this.getFragmentManager(), "timePicker");
+    }
+
+    public void showEndDatePickerDialog(View v) {
+        endDateFragment.show(this.getFragmentManager(), "datePicker");
+    }
+
+
 
     /**
      * Method gets triggered when Register button is clicked
@@ -57,16 +98,25 @@ public class RegisterEvent extends Activity {
      * @param view
      */
     public void submit(View view) {
+        start += startDateFragment.getDate();
+
+        end += endDateFragment.getDate();
+
+        start += " at " + startTimeFragment.getTime();
+        startTime_button.setText(start);
+
+        end += " at " + endTimeFragment.getTime();
+        endTime_button.setText(end);
+
+
         String eventName = name.getText().toString();
         String description = desc.getText().toString();
-        String startTime = start.getText().toString();
-        String endTime = end.getText().toString();
         RequestParams params = new RequestParams();
         /*  We don't need to check if parameters are correct, they are checked on the server side.  */
         params.put("name", eventName);
         params.put("desc", description);
-        params.put("start", startTime);
-        params.put("end", endTime);
+        params.put("start", start);
+        params.put("end", end);
         invokeWS(params);
     }
 
