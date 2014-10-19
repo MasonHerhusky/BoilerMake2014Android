b04f5317abf04e3674560d8a1d87e64c8c1e2725
Garrick <gbuckle@purdue.edu>
2014-10-18 21:20:38 -0400

    Added ability to log in and out.

--- src/com/purdueplaza/HomeActivity.java
+++ src/com/purdueplaza/HomeActivity.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
 
@@ -136,6 +139,31 @@ public class HomeActivity extends Activity{
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);  //On back button press on this page, close the app.
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                SharedPreferences preferences = getSharedPreferences("Login", 0);
+                preferences.edit().remove("key").commit();
+
+                Intent mainIntent = new Intent(getApplicationContext(),MyActivity.class);
+                Toast.makeText(getApplicationContext(), "You have successfully logged out.", Toast.LENGTH_LONG).show();
+                startActivity(mainIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
