172c77c80152c429b0b2460824f8b52022d91e71
masonherhusky <mason.herhusky@gmail.com>
2014-10-19 10:52:39 -0400

    changed background of attendance

--- src/com/purdueplaza/TimePickerFragment.java
+++ src/com/purdueplaza/TimePickerFragment.java
@@ -33,10 +33,17 @@ public class TimePickerFragment extends DialogFragment
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         if(hourOfDay > 12) {
             hourOfDay -= 12;
-            time = hourOfDay + ":" +  minute + " PM";
+            time += ""+ hourOfDay + ":";
+            if (minute == 0)
+                time += "0";
+            time +=  "" + minute + " PM";
         }
-        else
-            time = hourOfDay + ":" +  minute + " AM";
+        else {
+            time += "" + hourOfDay + ":";
+            if (minute == 0)
+                time += "0";
+            time += "" + minute + " AM";
+        }
     }
 
     public String getTime() {
